d94d932d202af13a60f041deccdc009fda3e23b2
DF4ze <df4ze@free.fr>
2015-09-14 23:48:57 +0200

    Intégration du capteur ultrason et servo balai (erreur sur le dernier commit)

--- ArduiBotNewGeneration/ArduiBotNewGeneration.ino
+++ ArduiBotNewGeneration/ArduiBotNewGeneration.ino
@@ -16,10 +16,17 @@
 #define S_HORI_PIN		10 	// Pin
 #define S_VERT_PIN		11 	// Pin
 
+// Servo balai
+#define S_BALAI_PIN		8 	// Pin
+
 // Lumière(s)
 #define LIGHT_SPOT_PIN	52 	// pin de la lumière pour la caméra
 #define LIGHT_STROB_PIN	53 	// pin de la lumière pour le Stromboscope
 #define LIGHT_LAZER_PIN	7 	// pin de la lumière pour le Lazer
+
+// Ultrason
+#define US_TRIG_PIN		12 	
+#define US_ECHO_PIN		13 	
 //////////////////////
 
 //////////////////
@@ -46,6 +53,11 @@
 #define POS_MAX_HORI 		148
 #define POS_MIN_VERT 		50
 #define POS_MAX_VERT		147
+// Valeur d'angles pour le servo balai
+#define BALAI_POS_MAX		120
+#define BALAI_POS_MIN		60
+#define BALAI_NBSAUT		3 // doit-etre > 1 ... et impair
+
 ///////////////////
 
 
@@ -76,9 +88,15 @@ Servo tourV;
 // Lights
 int Lights [4] = {0,0,0,0};
 
-
-
-
+/////////////
+// UltraSon
+unsigned long stampTimeUS = millis();
+unsigned long checkingTimeUS = 1000;
+// Servo balai
+Servo balai;
+int posBalai = 90;
+bool sensBalai = true; // true sens horaire, false : anti-horaire
+int pasBalai = (BALAI_POS_MAX - BALAI_POS_MIN) / (BALAI_NBSAUT-1);
 
 
 
@@ -93,8 +111,10 @@ void setup() {
 
 	tourH.attach( S_HORI_PIN );  	// attache le servo horizontal au pin  
 	tourV.attach( S_VERT_PIN );  	// attache le servo vertical au pin  
-	Serial.println( "Declaration des Servos Tourelle" );
+	balai.attach( S_BALAI_PIN );  	// attache le servo balai au pin  
+	Serial.println( "Declaration des Servos" );
 
+	// Initialisation des pin pour les moteurs
 	pinMode( MG_S1_PIN, OUTPUT);
 	pinMode( MG_S2_PIN, OUTPUT);
 	pinMode( MG_V_PIN, OUTPUT);
@@ -102,16 +122,31 @@ void setup() {
 	pinMode( MD_S2_PIN, OUTPUT);
 	pinMode( MD_V_PIN, OUTPUT);
 	
+	// Initialisation des pin pour les lumières
 	pinMode( LIGHT_SPOT_PIN, OUTPUT);
 	pinMode( LIGHT_LAZER_PIN, OUTPUT);
 	pinMode( LIGHT_STROB_PIN, OUTPUT);
+	
+	  // Initialisation des pin pour le capteur UltraSon
+	pinMode(US_TRIG_PIN, OUTPUT);
+	pinMode(US_ECHO_PIN, INPUT);
+  
 	Serial.println( "Declaration des PINS" );
 	
 	Serial.println( "Ready !" );
 	Serial.println( "" );
 }
 
+
+/**
+ *
+ * Boucle Initiale
+ *
+ */
 void loop() {
+	unsigned long runningTime; // temps depuis le dernier checkingTimeUS
+	
+
  	if( stringComplete ){
 		// Pour test : Permet de savoir vraiment ce qui est retenu
 		if( Debug ){
@@ -135,10 +170,29 @@ void loop() {
 		inputString = "";
 		stringComplete = false;
 	} 
+	
+	// demande de récupération de la distance si on a dépassé le checkingTimeUS
+	runningTime = millis() - stampTimeUS;
+	if( runningTime > checkingTimeUS ){
+		stampTimeUS = millis();
+		// Serial.print("Distance : ");
+		// Serial.print( getDistanceUS() );
+		// Serial.println("cm");
+		
+		// On affiche la distance
+		actionStateUltraSonic();
+		// On demande au servo de bouger
+		actionServosBalai();
+	}
+	
 }
 
 
-
+/**
+ *
+ * Boucle reception Serial
+ *
+ */
 void serialEvent() {
 	// Tant que nous avons des données à réceptionner
 	while( Serial.available() && stringComplete == false ){
@@ -161,7 +215,11 @@ void serialEvent() {
 
 
 
-
+/**
+ *
+ * Gestion des parametres
+ *
+ */
 boolean parseParams( String params, int maxParams, int iaParams[], int& nbParams ){
     // Indicateur de validité des Params
     boolean ok = true;
@@ -216,7 +274,7 @@ boolean doAction( int iaParams[], int nbParams ){
 		iPosH = normaliseHori( iPosH );
 		iPosV = normaliseVert( iPosV );
 			
-		actionServos( iPosH, iPosV );
+		actionServosTourelle( iPosH, iPosV );
 		
 	}else if( iaParams[0] == MODE_EXTRA && nbParams == 4){ 
 		if( iaParams[1] == EXTRA_LIGHT ){
@@ -226,7 +284,7 @@ boolean doAction( int iaParams[], int nbParams ){
 		
 	}else if( iaParams[0] == MODE_STATE && nbParams == 3){ 
 		if( iaParams[1] == 0 && iaParams[2] == 0 )
-			actionFullState();
+			actionStateFull();
 			
 		else if( iaParams[1] == EXTRA_LIGHT ){
 			actionStateLight( iaParams[2] );
@@ -240,7 +298,11 @@ boolean doAction( int iaParams[], int nbParams ){
 }
 
 
-
+/**
+ *
+ * Fonction de vérification des données entrées
+ *
+ */
 int normalisePWM(int pwm){
 	if( pwm > MAX_PWM )
 		pwm = MAX_PWM;
@@ -269,7 +331,11 @@ int normaliseVert(int iPosV){
 
 
 
-
+/**
+ *
+ * Actions sur les moteurs
+ *
+ */
 void actionMotors( int iVitesse, int iDelta ){
 	if( Debug ){
 		Serial.print( "Vitesse : " );
@@ -324,8 +390,12 @@ void actionMD( int iVitesse ){
 
 
 
-
-void actionServos( int iPosH, int iPosV ){
+/**
+ *
+ * Actions sur les servo
+ *
+ */
+void actionServosTourelle( int iPosH, int iPosV ){
 	if( Debug ){
 		Serial.print( "Hori : " );
 		Serial.print( iPosH );
@@ -339,6 +409,31 @@ void actionServos( int iPosH, int iPosV ){
 	tourV.write( iPosV );
 }
 
+void actionServosBalai( ){
+	int tempPas = ((sensBalai)?1:-1) * pasBalai;
+	
+	// on vérifie si le pas de plus dans le meme sens ne nous fait pas sortir des bornes
+	if( posBalai + tempPas < BALAI_POS_MIN || posBalai + tempPas > BALAI_POS_MAX ){
+		// si c le cas, on inverse
+		sensBalai = !sensBalai;
+		tempPas = ((sensBalai)?1:-1) * pasBalai;
+		
+		// si on déborde qd meme : pas = 0
+		tempPas = ( posBalai + tempPas < BALAI_POS_MIN || posBalai + tempPas > BALAI_POS_MAX )?0:tempPas;
+	}
+	
+	// on demande au servo de se mettre en position
+	posBalai += tempPas;
+	balai.write( 180 - posBalai );
+	
+	// si on est sur la valeur exacte, on change de sens
+}
+
+/**
+ *
+ * Actions sur les lumières
+ *
+ */
 void actionLights( int iLight, int iValue ){
 	if( Debug ){
 		Serial.print( "Light : " );
@@ -361,9 +456,46 @@ void actionLights( int iLight, int iValue ){
 	}
 	Lights[iLight] = iValue;
 }
+
+
+/**
+ *
+ * Capteur Ultrason
+ *
+ */
+int getDistanceUS(){
+	int duration, distance;
+	
+	// Initialisation du capteur
+	digitalWrite(US_TRIG_PIN, HIGH);
+	delayMicroseconds(1000);
+	digitalWrite(US_TRIG_PIN, LOW);
+	// Mesure du temps : allé/retour de l'ultrason
+	duration = pulseIn(US_ECHO_PIN, HIGH, 12000); // l'ultrason ne peut mesuré précisément que jusqu'a 4-5m, je mets un timeout pour 10m
+	// conversion en distance.
+	distance = (duration/2) * 0.03495;
+	
+	// s'il y a timeout, duration va etre = 0 donc on met la distance a 10m
+	if( duration == 0 )
+		distance = 1000;
+	
+	return distance;
+}
+
+
+
+
+
+
+
+/**
+ *
+ * Retour d'états
+ *
+ */
 	/**
-	 * Sserial doit etre de la forme :
-	 * [data]	[sensor]	[distance]		int
+	 * serial doit etre de la forme :
+	 * [data]	[sensor]	[distance]		int(pos).int(distance)
 	 * 						[lumiere]		int
 	 * 						[temperature]	int
 	 * 
@@ -374,7 +506,7 @@ void actionLights( int iLight, int iValue ){
 	 * @param sSerial
 	 * @return
 	 */
-void actionFullState(){
+void actionStateFull(){
 	actionStateLight( LIGHT_LAZER );
 	actionStateLight( LIGHT_SPOT );
 	actionStateLight( LIGHT_STROB );
@@ -391,6 +523,13 @@ void actionStateLight( int iLight ){
 	}
 	Serial.println( Lights[iLight] );
 
+}
+
+void actionStateUltraSonic( ){
+	Serial.print( "[data][sensor][distance]" );
+	Serial.print( posBalai );
+	Serial.print( "." );
+	Serial.println( getDistanceUS() );
 }
 
 
